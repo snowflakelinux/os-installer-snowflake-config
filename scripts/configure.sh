cf2efc3b475fb3d76f7e9436682924af3f2d4407
Victor Fuentes <vmfuentes64@gmail.com>
2022-12-10 18:20:57 -0500

    Fix flake config

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -110,26 +110,11 @@ fi
 FLAKETXT="{
   inputs = {
     nixpkgs.url = \"github:NixOS/nixpkgs/nixos-unstable\";
-    snowflake = {
-      url = \"github:snowflakelinux/snowflake-modules\";
-      inputs.nixpkgs.follows = \"nixpkgs\";
-    };
-    nix-data = {
-      url = \"github:snowflakelinux/nix-data\";
-      inputs.nixpkgs.follows = \"nixpkgs\";
-    };
-    nix-software-center = {
-      url = \"github:vlinkz/nix-software-center\";
-      inputs.nixpkgs.follows = \"nixpkgs\";
-    };
-    nixos-conf-editor = {
-      url = \"github:vlinkz/nixos-conf-editor\";
-      inputs.nixpkgs.follows = \"nixpkgs\";
-    };
-    snow = {
-      url = \"github:snowflakelinux/snow\";
-      inputs.nixpkgs.follows = \"nixpkgs\";
-    };
+    snowflake.url = \"github:snowflakelinux/snowflake-modules\";
+    nix-data.url = \"github:snowflakelinux/nix-data\";
+    nix-software-center.url = \"github:vlinkz/nix-software-center\";
+    nixos-conf-editor.url = \"github:vlinkz/nixos-conf-editor\";
+    snow.url = \"github:snowflakelinux/snow\";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
